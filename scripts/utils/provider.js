d9fdb4dc8eacb2e80a87b9ed6184e45c72f162b2
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-03-15 04:25:45 +0100

    Added infura address to an environment variable

--- scripts/utils/provider.js
+++ scripts/utils/provider.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const ethers = require("ethers");
 
 //Connect to ropsten testnet to user fake ether; Infura will be used to run our eth node and expose it with the URL
 const provider = ethers.getDefaultProvider("ropsten", {
-  infura: "https://ropsten.infura.io/v3/bb5f27d660ef40aeba3238e77c120cd5",
+  infura: process.env.INFURA_ADDRESS,
 });
 
 module.exports = provider;
